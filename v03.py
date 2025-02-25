f5cdc67d4b27304f5841dc96d411427c1d6661cc
fkarabacakli <hfkarabacakli20@gmail.com>
2025-02-25 15:58:50 +0300

    fps added

--- v03.py
+++ v03.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import cv2 as cv
+import time
 
 
 def get_hsv_bounds(rgb_color, hue_range=10, sat_min=100, val_min=50):
@@ -23,32 +24,32 @@ def contour_corner(contour):
 def main():
     cap = cv.VideoCapture("comb.MOV")
 
-    #rgb_color = (252, 139, 50)
-    #rgb_color = (215,202,73) /yellow
-    rgb_color1 = (219,66,70) #red
-    rgb_color2 = (16,75,148) #blue
-        
-    lower_blue1, upper_blue1 = get_hsv_bounds(rgb_color1)  
-    lower_blue2, upper_blue2 = get_hsv_bounds(rgb_color2)  
-    while True:
+    rgb_color1 = (219, 66, 70)  # red
+    rgb_color2 = (16, 75, 148)  # blue
 
+    lower_blue1, upper_blue1 = get_hsv_bounds(rgb_color1)
+    lower_blue2, upper_blue2 = get_hsv_bounds(rgb_color2)
+    
+    prev_time = time.time()
+
+    while True:
         ret, frame = cap.read()
-        
         if not ret:
             break
+
+        current_time = time.time()
+        fps = 1 / (current_time - prev_time)
+        prev_time = current_time
 
         org = frame.copy()
         hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
-        
         mask1 = cv.inRange(hsv, lower_blue1, upper_blue1)
         mask2 = cv.inRange(hsv, lower_blue2, upper_blue2)
 
-        #bitwise = cv.bitwise_and(frame, frame, mask=mask)
-
         mask = cv.bitwise_or(mask1, mask2)
         contours, _ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-        
+
         for contour in contours:
             area = cv.contourArea(contour)
             if area > 1000:
@@ -57,10 +58,12 @@ def main():
                 x, y, w, h = cv.boundingRect(contour)
                 cv.putText(frame, f"Corners: {corner}", (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
 
+        # FPS ekleme
+        cv.putText(frame, f"FPS: {int(fps)}", (10, 40), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
         cv.imshow("Frame", frame)
         cv.imshow("Org", org)
         cv.imshow("Mask", mask)
-        #cv.imshow("Bitwise", bitwise)
 
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
